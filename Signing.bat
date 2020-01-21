2243cee2c887b33385e4575ddb55863954fa64d3
vnoves <valentin.noves@engworks.com>
2020-01-20 22:11:56 -0300

    added automatic signing

--- Signing.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-pushd "%~dp0"
-if exist cs (goto okcs) else (echo "No cs folder found." && goto exit)
-:okcs
-SignTool sign /f "path to your PFX file" /p "your PFX file password" /tr http://timestamp.comodoca.com  /td %cd%/bin/Debug/
-:exit
